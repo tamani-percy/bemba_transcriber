b9524be1d1b9a9906dd69df4a02bf9ca6af18893
Tamani <102812813+tamani-percy@users.noreply.github.com>
2025-07-10 20:03:02 +0200

    Update bemba_api.dart

--- lib/bemba_api.dart
+++ lib/bemba_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 Future<String> bembaTranscription(String filename) async {
   const apiUrl = "https://api-inference.huggingface.co/models/csikasote/wav2vec2-large-xls-r-300m-bemba-fds";
   final headers = {
-    "Authorization": "Bearer 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",
+    "Authorization": "Bearer ",
   };
 
   var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
